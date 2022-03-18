8c953897a4175507e5bc4b16087732daeda2dd30
SRodrigues <srodrigues1497@gmail.com>
2022-03-18 23:28:15 +0530

    script updated

--- Assignment4/test.sh
+++ Assignment4/test.sh
@@ -5,7 +5,7 @@ echo "Enter the new string"
 read str_2
 echo "Replace zip files(yes/no)"
 read str_3
-echo "Enter filename"
+echo "Enter filename which has list of directories to be excluded"
 read str_4
 foo=()
 while read fol;
@@ -17,7 +17,7 @@ do
   ITER=0
   for Item in ${foo[*]}
   do
-    tmpstr+="! -path '.$Item/*' "
+    tmpstr+="! -path '*/$Item/*' "
    ITER+=1
  done
 
@@ -32,6 +32,7 @@ do
   echo $replace_in_zip_file
   echo $str_1
   echo $str_2
+  echo find . -type f ! -iname *.sh $replace_in_zip_file ! -path ./dir_1 ! -path ./dir_2 -exec sed -i 's/'$str_1'/'$str_2'/g' {} +
   echo find . -type f ! -iname *.sh $replace_in_zip_file $tmpstr -exec sed -i 's/'$str_1'/'$str_2'/g' {} +
   find . -type f ! -iname *.sh $replace_in_zip_file $tmpstr -exec sed -i 's/'$str_1'/'$str_2'/g' {} +
 
